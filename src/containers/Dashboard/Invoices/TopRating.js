06cd4cf3b58967951d7045a3df936c083fcdfbb9
Marwa92 <engmarwayehya@yahoo.com>
2020-10-03 23:03:55 +0200

    create top rating section

---
+++ src/containers/Dashboard/Invoices/TopRating.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from "react";
+import CustomTabs from "../../../components/CustomTabs";
+
+export default function TopRating() {
+  return (
+    <Fragment>
+      <CustomTabs />
+    </Fragment>
+  );
+}
